da8aa95d63ab5b42a5f9363c232157906ca2f932
Xys-Tus <Sixtus Onyedibe>
2023-09-25 22:18:35 +0100

    Fixed the error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
+    const task = false;
+    const task2 = true;
   }
 
   return [task, task2];
