38c5b65fe1530294b9a5a30fa7501fcdbfc1b3c6
Xys-Tus <Sixtus Onyedibe>
2023-09-25 22:14:43 +0100

    Block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
-    }
-  
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = false;
+    task2 = true;
   }
-  
+
+  return [task, task2];
+}
