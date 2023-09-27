ad8a76ede2faa127740c56d08fc1abbe9f048448
Xys-Tus <Sixtus Onyedibe>
2023-09-27 23:15:18 +0100

    eturn a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,3 @@
-export default function getResponseFromAPI(){
-    return new Promise((resolve, reject) => resolve);
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => resolve);
 }
