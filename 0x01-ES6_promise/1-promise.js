6b99664a14d40a97d8da0b03921b918fe18bc90a
Xys-Tus <Sixtus Onyedibe>
2023-09-27 23:34:02 +0100

    Fixed eslint error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,12 @@
 export default function getResponseFromAPI(passed) {
-    return new Promise((resolve, reject) => {
-      if (!passed) {
-        reject(new Error('The fake API is not working currently'));
-      } else {
-        resolve({
-          status: 200,
-          body: 'Success',
-        });
-      }
-    });
-  }
-  
+  return new Promise((resolve, reject) => {
+    if (!passed) {
+      reject(new Error('The fake API is not working currently'));
+    } else {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    }
+  });
+}
