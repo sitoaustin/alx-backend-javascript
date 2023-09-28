318a3ff51b0a82d48e773edd81bfe4e222c66338
Xys-Tus <Sixtus Onyedibe>
2023-09-28 09:21:43 +0100

    Try catch finally

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,5 +1,23 @@
-export default function guardrail(mathFunction) {
+function divideFunction(numerator, denominator) {
+  if (denominator === 0) throw new Error('cannot divide by 0');
+  try {
+    return numerator / denominator;
+  } catch (error) {
+    return error;
+  }
+}
+
+function guardrail(mathFunction) {
   const responses = [];
-  responses.push([mathFunction(), 'Guardrail was processed']);
+  try {
+    const response = mathFunction();
+    responses.push(response);
+  } catch (error) {
+    responses.push(`Error: ${error.message}`);
+  } finally {
+    responses.push('Guardrail was processed');
+  }
   return responses;
 }
+
+console.log(guardrail(() => divideFunction(10, 0)));
