1f8ffb8a82ddbdad42a2bee906f14fe23fd2c43e
Xys-Tus <Sixtus Onyedibe>
2023-09-28 18:03:22 +0100

    Updated the Error to TypeError

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new Error('Name must be a string');
+      throw new TypeError('Name must be a string');
     } else if (typeof length !== 'number') {
-      throw new Error('Length must be a number');
+      throw new TypeError('Length must be a number');
     } else if (typeof students !== 'object') {
-      throw new Error('Students must be an arrays of Strings');
+      throw new TypeError('Students must be an arrays of Strings');
     }
     students.forEach((student) => {
       if (typeof student !== 'string') throw new TypeError('Students must be an array of strings');
