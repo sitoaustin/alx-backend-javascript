2ea5eff02be4c5e32e4e08d4829d454aa34ff256
Xys-Tus <Sixtus Onyedibe>
2023-09-28 17:38:35 +0100

    Getter and setter JavaScript class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,38 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    } else if (typeof students !== 'object') {
+      throw new Error('Students must be an arrays of Strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
